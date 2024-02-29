a5212b516e8074bc091fffb49a4a72e2de28f0a9
simonsafhalter <simon.safhalter@masabi.com>
2024-02-29 13:17:40 +0100

    Add a unit test for utils.

---
+++ front-end/q4/client/src/utils/data-parsing.test.ts
@@ -0,0 +1,24 @@
+import { describe, it, expect } from "vitest";
+import { addRankToBooks } from "./data-parsing";
+import { Book } from "@/types/Book";
+
+describe("addRankToBooks", () => {
+  it("adds rank to each book based on its index", () => {
+    // Arrange
+    const books: Book = [
+      { title: "Book 1", author: "Author 1" },
+      { title: "Book 2", author: "Author 2" },
+      { title: "Book 3", author: "Author 3" },
+    ];
+
+    // Act
+    const rankedBooks = addRankToBooks(books);
+
+    // Assert
+    expect(rankedBooks).toEqual([
+      { title: "Book 1", author: "Author 1", rank: 1 },
+      { title: "Book 2", author: "Author 2", rank: 2 },
+      { title: "Book 3", author: "Author 3", rank: 3 },
+    ]);
+  });
+});
